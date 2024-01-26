ba61c0188fdbaa2fb5dea52035a08d21d26061be
ConnorNoddin <connor.noddin@maine.edu>
2024-01-26 12:26:15 -0500

    Testing PAPI

--- papi_test/papi_test.c
+++ papi_test/papi_test.c
@@ -2,13 +2,17 @@
 #include <stdio.h>
 
 /*
- * gcc -O2 -Wall -o papi_test papi_test.c -lpapi 
-*/
+ * gcc -O2 -Wall -o papi_test papi_test.c -lpapi
+ */
+
+#define NUM_EVENTS 2
 
 int main() {
   int retval;
+  int i;
   float rand = 1.363234643234;
-  long long count;
+  long long values[NUM_EVENTS];
+  float IPC;
 
   /* Initialize Papi */
   retval = PAPI_library_init(PAPI_VER_CURRENT);
@@ -25,15 +29,21 @@ int main() {
     fprintf(stderr, "Error creating eventset! %s\n", PAPI_strerror(retval));
   }
 
-  /* Add our measurements Here */
+  /* Add PAPI_TOT_CYC */
   retval = PAPI_add_named_event(eventset, "PAPI_TOT_CYC");
   if (retval != PAPI_OK) {
-    fprintf(stderr, "Error adding PAPI_TOT_CYC: %s\n", PAPI_strerror(retval));
+    printf("Trouble adding PAPI_TOT_CYC: %s\n", PAPI_strerror(retval));
   }
-  
+
+  /* Add PAPI_TOT_INS */
   retval = PAPI_add_named_event(eventset, "PAPI_TOT_INS");
   if (retval != PAPI_OK) {
-    fprintf(stderr, "Error adding PAPI_TOT_INS: %s\n", PAPI_strerror(retval));
+    printf("Trouble adding PAPI_TOT_INS: %s\n", PAPI_strerror(retval));
+  }
+
+  /* Remove processor from idle state */
+  for (i = 0; i < INT_MAX; i++) {
+    rand = rand / 1.001;
   }
 
   /* Begin Measurement */
@@ -42,20 +52,24 @@ int main() {
   if (retval != PAPI_OK) {
     fprintf(stderr, "Error starting CUDA: %s\n", PAPI_strerror(retval));
   }
-  
+
   /* Do some computation here */
-  for (int i = 0; i < 10000000; i++) {
-    rand = rand * 1.00000000001;
+  for (i = 0; i < INT_MAX; i++) {
+    rand = rand * 1.0001;
   }
   printf("Result is %f\n", rand);
 
   /* End Measurement */
-  retval = PAPI_stop(eventset, &count);
+  retval = PAPI_stop(eventset, values);
   if (retval != PAPI_OK) {
     fprintf(stderr, "Error stopping:  %s\n", PAPI_strerror(retval));
   } else {
-    printf("Measured %lld cycles\n", count);
+    printf("Measured %lld cycles\n", values[0]);
+    printf("Measured %lld instructions\n", values[1]);
   }
+
+  IPC = (values[1]*1.0) / (values[0]*1.0);
+  printf("Measured IPC is: %f\n", IPC);
 
   /* Cleanup */
   PAPI_cleanup_eventset(eventset);
